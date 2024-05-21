e4fd8c18c2a8519d972e0d8c09e9b15957612b6d
Miras Aliyev <aliyevmiras@gmail.com>
2024-05-22 01:06:21 +0500

    fix: change placeholders in login and register forms

--- CollectionsHub/Models/Account/RegisterViewModel.cs
+++ CollectionsHub/Models/Account/RegisterViewModel.cs
@@ -4,26 +4,22 @@ namespace CollectionsHub.Models.Account
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "Please provide a valid username")]
-        [Display(Prompt = "JohnDoe")]
+        [Required(ErrorMessage = "Username is required")]
         public required string UserName { get; set; }
 
 
-        [Required(ErrorMessage = "Please provide a valid email address")]
+        [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Please provide a valid email address")]
         [DataType(DataType.EmailAddress)]
-        [Display(Prompt = "johndoe@xxxxx.xxx")]
         public required string Email { get; set; }
 
         [Required(ErrorMessage = "Please provide a valid password")]
         [DataType(DataType.Password)]
-        [Display(Prompt = "Must have at least 1 character")]
         public required string Password { get; set; }
 
         [Required(ErrorMessage = "Please provide a valid password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match")]
         [DataType(DataType.Password)]
-        [Display(Prompt = "Confirm password")]
         public required string ConfirmPassword { get; set; }
     }
 }
