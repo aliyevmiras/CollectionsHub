50ad7b831bde41ad3d0dc21cc7becb8e894821f3
Miras Aliyev <aliyevmiras@gmail.com>
2024-05-29 23:35:23 +0500

    feat: add more seed data

--- CollectionsHub/Models/Data/SeedData.cs
+++ CollectionsHub/Models/Data/SeedData.cs
@@ -45,10 +45,47 @@ namespace CollectionsHub.Models.Data
                     addUser.PasswordHash = hashedPassword;
                     var result = await userManager.CreateAsync(addUser);
 
-                    var addCollection = new Collection { Author = addUser, Name = "New collection", Description = "New description", Category = addCategory };
+                    var addCollection = new Collection { Author = addUser, Name = "New collection1", Description = "New description", Category = addCategory };
                     context.Collections.Add(addCollection);
 
-                    context.Items.Add(new Item { Name = "TestItem", Collection = addCollection});
+                    context.Items.Add(new Item { Name = "TestItem1", Collection = addCollection});
+                    context.Items.Add(new Item { Name = "TestItem2", Collection = addCollection });
+                    context.Items.Add(new Item { Name = "TestItem3", Collection = addCollection });
+                    context.Items.Add(new Item { Name = "TestItem4", Collection = addCollection });
+                    context.Items.Add(new Item { Name = "TestItem5", Collection = addCollection });
+                    context.Items.Add(new Item { Name = "TestItem6", Collection = addCollection });
+                    context.Items.Add(new Item { Name = "TestItem7", Collection = addCollection });
+
+
+                    var secondCollection = new Collection { Author = addUser, Name = "New collection2", Description = "New description", Category = addCategory };
+                    context.Collections.Add(addCollection);
+                    context.Items.Add(new Item { Name = "TestItem1", Collection = secondCollection });
+
+                    var thirdCollection = new Collection { Author = addUser, Name = "New collection3", Description = "New description", Category = addCategory };
+                    context.Collections.Add(thirdCollection);
+                    context.Items.Add(new Item { Name = "TestItem1", Collection = thirdCollection });
+                    context.Items.Add(new Item { Name = "TestItem2", Collection = thirdCollection });
+                    context.Items.Add(new Item { Name = "TestItem3", Collection = thirdCollection });
+
+                    var fourthCollection = new Collection { Author = addUser, Name = "New collection4", Description = "New description", Category = addCategory };
+                    context.Collections.Add(fourthCollection);
+                    context.Items.Add(new Item { Name = "TestItem1", Collection = fourthCollection });
+                    context.Items.Add(new Item { Name = "TestItem2", Collection = fourthCollection });
+
+
+                    var fifthCollection = new Collection { Author = addUser, Name = "New collection5", Description = "New description", Category = addCategory };
+                    context.Collections.Add(fifthCollection);
+                    context.Items.Add(new Item { Name = "TestItem1", Collection = fifthCollection });
+                    context.Items.Add(new Item { Name = "TestItem2", Collection = fifthCollection });
+
+                    var sixthCollection = new Collection { Author = addUser, Name = "New collection6", Description = "New description", Category = addCategory };
+                    context.Collections.Add(sixthCollection);
+                    context.Items.Add(new Item { Name = "TestItem1", Collection = sixthCollection });
+                    context.Items.Add(new Item { Name = "TestItem2", Collection = sixthCollection });
+
+
+                    context.Collections.Add(addCollection);
+
 
                     await context.SaveChangesAsync();
                 }
