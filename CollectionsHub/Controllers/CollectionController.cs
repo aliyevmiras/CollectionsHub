27f790b98d7b03f7a4cf80e34251eb7920ede194
Miras Aliyev <aliyevmiras@gmail.com>
2024-06-04 06:13:48 +0500

    feat: add CollectionController with dummy action methods

---
+++ CollectionsHub/Controllers/CollectionController.cs
@@ -0,0 +1,47 @@
+﻿using CollectionsHub.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectionsHub.Controllers
+{
+    public class CollectionController : Controller
+    {
+        public ActionResult Details(Guid id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Collection collection)
+        {
+            return View();
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Guid id, Collection collection)
+        {
+            return View();
+        }
+
+        public ActionResult Delete(Guid id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Guid id, Collection collection)
+        {
+            return View();
+        }
+    }
+}
